014dbf7009952d227cc4841c17583d3b5b14f101
icdebruin <I.C.debruin@student.tudelft.nl>
2022-11-30 17:34:48 +0100

    Initial template

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+mod day1;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
+
+fn main() {
+    let day = 1;
+    match day {
+        1 => day1::day1(),
+        2 => day2::day2(),
+        3 => day3::day3(),
+        4 => day4::day4(),
+        5 => day5::day5(),
+        6 => day6::day6(),
+        7 => day7::day7(),
+        8 => day8::day8(),
+        9 => day9::day9(),
+        10 => day10::day10(),
+        11 => day11::day11(),
+        12 => day12::day12(),
+        13 => day13::day13(),
+        14 => day14::day14(),
+        15 => day15::day15(),
+        16 => day16::day16(),
+        17 => day17::day17(),
+        18 => day18::day18(),
+        19 => day19::day19(),
+        20 => day20::day20(),
+        21 => day21::day21(),
+        22 => day22::day22(),
+        23 => day23::day23(),
+        24 => day24::day24(),
+        25 => day25::day25(),
+        _ => {}
+    }
+}
