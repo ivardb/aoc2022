c9ea540c8388a1d4498b5f1c2198e40cae6204b7
icdebruin <I.C.debruin@student.tudelft.nl>
2022-12-01 09:21:13 +0100

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,14 @@
-pub fn day1() {
+use std::fs;
+use itertools::Itertools;
 
+pub fn day1() {
+    let data = fs::read_to_string("data/day1.txt").unwrap();
+    let elfs : Vec<_> = data.split("\n\n").collect();
+    let calories = elfs.iter()
+        .map(|s| s.split("\n")
+            .map(|s| s.parse::<u32>().unwrap())
+            .sum::<u32>())
+        .sorted_by(|i1, i2| i2.cmp(i1)).collect_vec();
+    println!("Part 1: {}", calories[0]);
+    println!("Part 2: {}", calories.iter().take(3).sum::<u32>())
 }
