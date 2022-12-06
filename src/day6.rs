e4782c364b7339b95ed82e9af124ae16c10204e1
icdebruin <I.C.debruin@student.tudelft.nl>
2022-12-06 09:52:11 +0100

    Day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,3 +1,26 @@
-pub fn day6() {
+use std::fs;
+use itertools::Itertools;
 
+fn process_data(data: String, length: usize) -> usize {
+    let mut buffer: Vec<char> = Vec::with_capacity(length);
+    buffer.extend(data[0..length].chars());
+    if buffer.iter().all_unique()  {
+        return length;
+    }
+    let mut i = 0;
+    for (j, c) in data.chars().enumerate().skip(length) {
+        buffer[i] = c;
+        if buffer.iter().all_unique() {
+            return j + 1;
+        }
+        i = (i + 1) % length;
+    }
+    return 0;
+}
+
+pub fn day6() {
+    let data = fs::read_to_string("data/day6.txt").unwrap();
+    println!("Part 1: {}", process_data(data, 4));
+    let data = fs::read_to_string("data/day6.txt").unwrap();
+    println!("Part 2: {}", process_data(data, 14));
 }
