b8de2962da43e066b2153f163e2cec942ce872c1
icdebruin <I.C.debruin@student.tudelft.nl>
2022-12-10 10:03:42 +0100

    Day 10

--- src/day10.rs
+++ src/day10.rs
@@ -1,3 +1,62 @@
-pub fn day10() {
+use std::collections::HashSet;
+use std::fs;
+use crate::day10::Instr::{AddX, NoOp};
 
+enum Instr {
+    NoOp,
+    AddX(i32),
+}
+
+pub fn day10() {
+    let data = fs::read_to_string("data/day10.txt").unwrap();
+    let mut x : i32 = 1;
+    let mut score = 0;
+    let mut delay = 0;
+    let mut iter = data.split_ascii_whitespace();
+    let iv = iter.next().unwrap();
+    let mut instr = match iv {
+        "noop" => {
+            delay = 1;
+            NoOp
+        },
+        _ => {
+            delay = 2;
+            AddX(iter.next().unwrap().parse().unwrap())
+        }
+    };
+    let counting_rounds : HashSet<_> = (20..=220).step_by(40).collect();
+    for round in 1.. {
+        let draw_pos = (round - 1) % 40;
+        if draw_pos == 0 {
+            println!()
+        }
+        if x.abs_diff(draw_pos) <= 1 {
+            print!("#");
+        } else {
+            print!(".")
+        }
+        if counting_rounds.contains(&round) {
+            score += round * x;
+        }
+        delay -= 1;
+        if delay == 0 {
+            match instr {
+                NoOp => {}
+                AddX(dx) => {x += dx}
+            }
+            if let Some(iv) = iter.next() {
+                instr = match iv {
+                    "noop" => {
+                        delay = 1;
+                        NoOp
+                    },
+                    _ => {
+                        delay = 2;
+                        AddX(iter.next().unwrap().parse().unwrap())
+                    }
+                }
+            } else {break}
+        }
+    }
+    println!("\nPart 1: {score}");
 }
--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ mod day24;
 mod day25;
 
 fn main() {
-    let day = 9;
+    let day = 10;
     match day {
         1 => day1::day1(),
         2 => day2::day2(),
